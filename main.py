979a5f43496000b0eb90fbaf89e27b615fd48a82
lzm-03 <167625394+lzm-01@users.noreply.github.com>
2025-12-29 20:47:25 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils.config import get_config
+from solver.solver import Solver
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='N_SR')
+    parser.add_argument('--option_path', type=str, default='/home/lizhuomao/Contrast4/restruct/MSDDN-main/option.yml')
+    opt = parser.parse_args()
+    cfg = get_config(opt.option_path)
+    solver = Solver(cfg)
+    solver.run()
